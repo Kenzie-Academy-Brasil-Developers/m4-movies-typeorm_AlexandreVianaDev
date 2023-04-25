d6a28f91bf56379b6841b486feffe26fabd75be9
AlexandreVianaDev <alexandreviana94@hotmail.com>
2023-04-25 14:29:37 -0300

    feat: pagination

--- src/controllers/movies.controllers.ts
+++ src/controllers/movies.controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { createMovieService } from "../services/createMovie.services";
 import { Movie } from "../entities";
 import { listMoviesService } from "../services/listMovies.services";
+import { TMovieListResponse } from "../interfaces/movies.interfaces";
 
 export const createMovieController = async (
   req: Request,
@@ -15,6 +16,21 @@ export const listMoviesController = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const movies: Movie[] = await listMoviesService();
+  const pageQuery: any = Number(req.query.page) || 1;
+  const perPageQuery: any = Number(req.query.perPage) || 5;
+  const sortQuery: any = req.query.sort || "id";
+  const orderQuery: any = req.query.order || "asc";
+  const url = `${req.protocol}://${req.hostname}${req.originalUrl}`;
+
+  console.log(url)
+
+  const movies: TMovieListResponse = await listMoviesService(
+    pageQuery,
+    perPageQuery,
+    sortQuery,
+    orderQuery,
+    url
+  );
+
   return res.status(200).json(movies);
 };
--- src/services/listMovies.services.ts
+++ src/services/listMovies.services.ts
@@ -1,8 +1,81 @@
 import { Repository } from "typeorm";
 import { AppDataSource } from "../data-source";
 import { Movie } from "../entities";
+import { TMovieListResponse } from "../interfaces/movies.interfaces";
+import "dotenv/config";
+import { PORT } from "../server";
 
-export const listMoviesService = async (): Promise<Movie[]> => {
-  const userRepo: Repository<Movie> = AppDataSource.getRepository(Movie);
-  return await userRepo.find();
+export const listMoviesService = async (
+  pageQuery: any,
+  perPageQuery: any,
+  sortQuery: any,
+  orderQuery: any,
+  url: string
+): Promise<TMovieListResponse> => {
+  const moviesRepo: Repository<Movie> = AppDataSource.getRepository(Movie);
+
+  // console.log(pageQuery, perPageQuery, sortQuery, orderQuery);
+
+  if (perPageQuery < 0 || perPageQuery > 5) {
+    perPageQuery = 5;
+  }
+
+  if (pageQuery <= 0) {
+    pageQuery = 1;
+  }
+
+  let movies: Movie[] = [];
+
+  const allMovies: Movie[] = await moviesRepo.find();
+
+  if (sortQuery === "id") {
+    movies = await moviesRepo.find({
+      order: { id: orderQuery },
+      skip: (pageQuery - 1) * perPageQuery,
+      take: perPageQuery,
+    });
+  }
+
+  if (sortQuery === "price") {
+    movies = await moviesRepo.find({
+      order: { price: orderQuery },
+      skip: (pageQuery - 1) * perPageQuery,
+      take: perPageQuery,
+    });
+  }
+
+  if (sortQuery === "duration") {
+    movies = await moviesRepo.find({
+      order: { duration: orderQuery },
+      skip: (pageQuery - 1) * perPageQuery,
+      take: perPageQuery,
+    });
+  }
+
+  let urlPrevPage = null;
+  let urlNextPage = null;
+
+  // console.log("PAGINATION", pageQuery, perPageQuery);
+  // console.log("cALCULO", movies.length / perPageQuery);
+
+  if (pageQuery !== 1) {
+    urlPrevPage = `https://localhost:${PORT}/movies/?page=${
+      pageQuery - 1
+    }&perPage=${perPageQuery}&sort=${sortQuery}&order=${orderQuery}`;
+  }
+
+  if (allMovies.length / perPageQuery > pageQuery) {
+    urlNextPage = `https://localhost:${PORT}/movies/?page=${
+      pageQuery + 1
+    }&perPage=${perPageQuery}&sort=${sortQuery}&order=${orderQuery}`;
+  }
+
+  const response = {
+    prevPage: urlPrevPage,
+    nextPage: urlNextPage,
+    count: allMovies.length,
+    data: movies,
+  };
+
+  return response;
 };
